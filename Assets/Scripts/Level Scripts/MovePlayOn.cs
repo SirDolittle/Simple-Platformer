49311036872b675458f43bb56f0d0c9f529e6ef6
SirDolittle <noahjb1999@gmail.com>
2019-11-21 15:43:33 +0000

    Level 3 updates

--- Assets/Scripts/Level Scripts/MovePlayOn.cs
+++ Assets/Scripts/Level Scripts/MovePlayOn.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class MovePlayOn : MonoBehaviour
 {
-    private GameObject target = null;
-    private Vector3 offset;
-    void Start()
-    {
-        target = null;
-    }
-    void OnTriggerStay2D(Collider2D col)
-    {
-        target = col.gameObject;
-        offset = target.transform.position - transform.position;
-    }
-    void OnTriggerExit2D(Collider2D col)
-    {
-        target = null;
-    }
-    void LateUpdate()
-    {
-        if (target != null)
-        {
-            target.transform.position = transform.position + offset;
-        }
-    }
+ private GameObject target=null;
+ private Vector3 offset;
+ void Start(){
+     target = null;
+ }
+ void OnTriggerStay2D(Collider2D col){
+     target = col.gameObject;
+     offset = target.transform.position - transform.position;
+ }
+ void OnTriggerExit2D(Collider2D col){
+     target = null;
+ }
+ void LateUpdate(){
+     if (target != null) {
+         target.transform.position = transform.position+offset;
+     }
+ }
 }
